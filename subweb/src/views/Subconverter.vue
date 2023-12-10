4e7b7e5f559f5e6464c70ca76486ca489fffaa24
马可 <78850005+makebl@users.noreply.github.com>
2023-12-10 23:41:40 +0800

    Update Subconverter.vue

--- subweb/src/views/Subconverter.vue
+++ subweb/src/views/Subconverter.vue
@@ -474,7 +474,7 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-          "本地自定义后端【vless+hysteria】": "http://ip:25500"
+          "本地自定义后端【vless+hysteria】": "http://127.0.0.1:25500"
           "肥羊增强型后端【vless+hysteria】": "https://api.v1.mk",
           "肥羊备用后端【vless+hysteria】": "https://sub.d1.mk",
           "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
@@ -484,7 +484,7 @@ export default {
           "sub作者&lhie1提供": "https://api.dler.io",
         },
         backendOptions: [
-          {value: "http://ip:25500"},
+          {value: "http://127.0.0.1:25500"},
           {value: "https://api.v1.mk"},
           {value: "https://sub.d1.mk"},
           {value: "https://api.tsutsu.one"},
