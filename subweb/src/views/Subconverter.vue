121155980eeca0eb2de3c64c9249f5afc222b275
马可 <78850005+makebl@users.noreply.github.com>
2023-12-12 13:18:51 +0800

    Update Subconverter.vue

--- subweb/src/views/Subconverter.vue
+++ subweb/src/views/Subconverter.vue
@@ -251,15 +251,6 @@
                 >从URL解析
                 </el-button>
               </el-form-item>
-              <el-form-item label-width="0px" style="text-align: center">
-                <el-button
-                    style="width: 250px;"
-                    type="success"
-                    icon="el-icon-video-play"
-                    @click="centerDialogVisible = true"
-                >视频教程
-                </el-button>
-              </el-form-item>
             </el-form>
           </el-container>
         </el-card>
